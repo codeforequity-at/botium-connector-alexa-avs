c110a93dd673ea3842da87ac6ba1f0105dc48ce0
Attila Ujj <attila.ujj@botium.at>
2018-12-18 10:45:03 +0100

    cleaner solution implemented

--- index.js
+++ index.js
@@ -104,11 +104,11 @@ const _mp3ToWav = (mp3AsBuffer) => {
   // or just the HTTP message is not parsed well?
   // this MP3 is correct for a MP3 player, but not for mp3ToAudioBuffer.isMP3 module.
   // so i remove this \n\r
-  mp3AsBuffer = mp3AsBuffer.slice(2, mp3AsBuffer.length)
-  if (mp3AsBuffer[0] !== 73) {
+  if (mp3AsBuffer[2] !== 73) {
     debug('Error: The source does not looks as an mp3!!!')
-    return Promise.reject(new Error('The source does not looks as an mp3'))
+    return Promise.reject(new Error(`The source does not looks as an mp3 [${mp3AsBuffer[0]}] [${mp3AsBuffer[1]}] [${mp3AsBuffer[2]}] `))
   }
+  mp3AsBuffer = mp3AsBuffer.slice(2, mp3AsBuffer.length)
   // 2 mp3Buffer -> audioBuffer (It is a generalized audio format?)
   // 3 audioBuffer -> wav array buffer (third buffer type in this function. But function wants it)
   // 4 wav array buffer -> wav buffer (buffer can be stored as file. Or wav-array-buffer could be written too?)
