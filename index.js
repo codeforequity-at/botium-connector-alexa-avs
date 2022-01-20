4f2999fc6e03ba07a7228c2e72765dbb81979bf1
Florian Treml <florian.treml@botium.at>
2022-01-20 08:27:19 +0100

    Added capability descriptors for speech

--- index.js
+++ index.js
@@ -167,5 +167,29 @@ class BotiumConnectorAlexaAvs {
 
 module.exports = {
   PluginVersion: 1,
-  PluginClass: BotiumConnectorAlexaAvs
+  PluginClass: BotiumConnectorAlexaAvs,
+  PluginDesc: {
+    name: 'Alexa Voice Service',
+    provider: 'Botium',
+    features: {
+      conversationFlowTesting: false,
+      e2eTesting: true,
+      audioInput: true,
+      supportedFileExtensions: ['.wav']
+    },
+    capabilities: [
+      {
+        name: 'ALEXA_AVS_STT',
+        label: 'Speech Recognition Profile',
+        type: 'speechrecognitionprofile',
+        required: false
+      },
+      {
+        name: 'ALEXA_AVS_TTS',
+        label: 'Speech Synthesis Profile',
+        type: 'speechsynthesisprofile',
+        required: false
+      }
+    ]
+  }
 }
